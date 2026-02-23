dfa202cb434669d47b10f400ca8915426e3e0194
Cyon <1900236964@qq.com>
2026-02-23 11:21:18 +0800

    refactor(router): 将历史模式从webHistory改为webHashHistory
    修改路由历史模式以避免生产环境部署时的路径问题，使用hash模式更兼容不同服务器配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { storage } from '@/utils/storage'
 
 const routes = [
@@ -50,7 +50,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/apihub'),
+  history: createWebHashHistory ('/apihub'),
   routes
 })
 
